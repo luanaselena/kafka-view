faca83ba7b330762cf368ac8483f8538b1486e2c
luanaselena <luanaselenatraversa@gmail.com>
2021-09-22 20:31:20 -0300

    notificaciones completas

--- cliente-kafka/src/components/ui/Notificaciones.js
+++ cliente-kafka/src/components/ui/Notificaciones.js
@@ -7,53 +7,78 @@ import { useHistory } from "react-router-dom";
 import Navegacion from "./Navegacion";
 
 const useStyles = makeStyles((theme) => ({
-	root: {
-		flexGrow: 1,
-	},
+    root: {
+        flexGrow: 1,
+    },
 }));
 
 const Notificaciones = () => {
     const classes = useStyles();
-	let history = useHistory();
+    let history = useHistory();
 
     const usernameSesion = localStorage.getItem("usuario");
-	//Si el usuario no esta logueado no puede entrar a la pagina
-	if (usernameSesion === "" || usernameSesion === undefined){
-		history.push("/signin");
-	}
+    //Si el usuario no esta logueado no puede entrar a la pagina
+    if (usernameSesion === "" || usernameSesion === undefined) {
+        history.push("/signin");
+    }
 
-    const [notifLikes, setNotifLikes] = useState([]);
-    const [notifFollowers, setFollowers] = useState([]);
+    const [likes, setLikes] = useState([]);
+    const [followers, setFollowers] = useState([]);
 
     const fetchApi = async () => {
-		const notifLikes = await apiAxios.get("http://localhost:8080/likes");
-		setNotifLikes(notifLikes.data);
+        const likes = await apiAxios.get("http://localhost:8080/likes", {
+            params: { username: usernameSesion },
+        });
+        setLikes(likes.data);
 
-		const notifFollowers = await apiAxios.get("http://localhost:8080/getFollowers");
-		setFollowers(notifFollowers.data);
-
-	};
+        const followers = await apiAxios.get(
+            "http://localhost:8080/getFollowers",
+            {
+                params: { username: usernameSesion },
+            }
+        );
+        setFollowers(followers.data);
+    };
 
     useEffect(() => {
-		fetchApi();
-	// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, []);
+        fetchApi();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
 
-    return(
-<div className={classes.root}>
-			<Container maxWidth="md">
-				<Grid container spacing={3}>
-					<Grid item xs={12}>
-						<h1 className={classes.paper}>lista notis</h1>
-					</Grid>
-					<Grid item xs={12}>
-						
-					</Grid>
-				</Grid>
-			</Container>
-		</div>
+    console.log("Followers");
+    console.log(followers);
+    console.log("Likes");
+    console.log(likes);
+
+    return (
+        <div className={classes.root}>
+            <Container maxWidth="md">
+                <Grid container spacing={3}>
+                    <Grid item xs={12}>
+                        <h1 className={classes.paper}>lista notis</h1>
+                    </Grid>
+                    <Grid item xs={6}>
+						{followers === undefined || followers.length === 0 ? (
+							<p>No hay notificaciones nuevas</p>
+						) : (
+							followers.map((follower) => (
+								<div>{follower.username} te siguio</div>
+							))
+						)}
+                    </Grid>
+                    <Grid item xs={6}>
+						{likes === undefined || likes.length === 0 ? (
+							<p>No hay notificaciones nuevas</p>
+						) : (
+							likes.map((like) => (
+								<div>{like.userLike.username} te dio like en el post "{like.post.title}"</div>
+							))
+						)}
+                    </Grid>
+                </Grid>
+            </Container>
+        </div>
     );
-
 };
 
 export default Notificaciones;
